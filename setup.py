049e50bd5ab2d1ec950ca8d4fa11d32499ff717a
James <jjfeore>
2017-05-22 14:07:20 -0700

    client, tox, and setup.py finished

---
+++ setup.py
@@ -0,0 +1,28 @@
+"""Setup for Echo Serr."""
+
+
+from setuptools import setup
+
+
+extra_packages = {
+    'testing': ['ipython', 'pytest', 'pytest-watch', 'pytest-cov', 'tox', 'socket']
+}
+
+
+setup(
+    name='echo',
+    desctription='Client sends a msg, receives it back, and then returns it.',
+    version='0.1',
+    author='Ophelia Yin, James Feore',
+    author_email='yo@whatup.com, sup@dawg.com',
+    license='MIT',
+    py_modules=['client', 'server'],
+    package_dir={'': 'src'},
+    install_requires=[],
+    extras_require=extra_packages,
+    entry_points={
+        'console_scripts': [
+            'client = client:client'
+        ]
+    }
+)
---
+++ src/client.py
@@ -0,0 +1,37 @@
+"""Simple Echo Socket Client."""
+
+
+import socket
+
+
+def client(msg):
+    """Send server a message, receives another msg, and returns it."""
+    infos = socket.getaddrinfo('127.0.0.1', 5000)
+    stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    cli_sock = client.connect(stream_info[-1])
+    msg = append_len(msg).encode('utf8')
+    cli_sock.sendall(msg)
+    cli_sock.shutdown(socket.SHUT_WR)
+    buffsize = 8
+    response = ''
+    msg_len = int(cli_sock.recv(buffsize))
+    n = 0
+    while n < msg_len:
+        response += cli_sock.recv(buffsize)
+        n += buffsize
+    cli_sock.shutdown(socket.SHUT_RD)
+    cli_sock.close()
+    return response.decode('utf8')
+
+
+def append_len(msg):
+    """Take a msg and adds a str length to the front of the str."""
+    extra_zeros = 8 - len(str(len(msg)))
+    return ('0' * extra_zeros) + msg
+
+
+if __name__ == '__main__':  # pragma: no cover
+    msg = 'Hello. How are you doing?'
+    print('Sent: ' + msg)
+    print('Received: ' + client(msg))
