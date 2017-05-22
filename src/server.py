2372f301030d4edb4c3706a7890369df26550a06
James <jjfeore>
2017-05-22 15:46:40 -0700

    All tests pass

--- src/server.py
+++ src/server.py
@@ -3,30 +3,36 @@ import socket
 import sys
 
 
-def server():
+def server():  # pragma: no cover
         """Echo server."""
-        try:
-            echo_server_sock = socket.socket(socket.AF_INET,
-                                             socket.SOCK_STREAM,
-                                             socket.IPPROTO_TCP)
-            address = ('127.0.0.1', 5001)
-            echo_server_sock.bind(address)
-            echo_server_sock.listen(1)
-            conn, addr = echo_server_sock.accept()
-            buffsize = 8
-            response = ""
-            msg_len = int(conn.recv(buffsize))
-            n = 0
-            while n < msg_len:
-                response += conn.recv(buffsize).decode('utf8')
-                n += buffsize
-            response = append_len(response)
-            conn.sendall(response.encode('utf8'))
-            conn.close()
-            echo_server_sock.close()
-        except KeyboardInterrupt:
-            echo_server_sock.close()
-            sys.exit(0)
+        echo_server_sock = socket.socket(socket.AF_INET,
+                                         socket.SOCK_STREAM,
+                                         socket.IPPROTO_TCP)
+        address = ('127.0.0.1', 5005)
+        echo_server_sock.bind(address)
+        echo_server_sock.listen(1)
+        while True:
+            try:
+                conn, addr = echo_server_sock.accept()
+                buffsize = 8
+                response = ""
+                msg_len = conn.recv(buffsize).decode('utf8')
+                if msg_len is not '':
+                    print(msg_len)
+                    msg_len = int(msg_len)
+                    n = 0
+                    while n < msg_len:
+                        response += conn.recv(buffsize).decode('utf8')
+                        n += buffsize
+                    response = append_len(response)
+                    conn.sendall(response.encode('utf8'))
+                    conn.close()
+                else:
+                    continue
+            except KeyboardInterrupt:
+                break
+        echo_server_sock.close()
+        sys.exit(0)
 
 
 def append_len(msg):
