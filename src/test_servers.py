5105b158f34e3d9db686793cc6d3a92b807ad31e
James <jjfeore>
2017-05-24 09:54:11 -0700

    Reformatted to include carriage returns

--- src/test_servers.py
+++ src/test_servers.py
@@ -14,36 +14,20 @@ TEST_ECHO = [
 ]
 
 
-TEST_APPEND = [
-    ('Hey, how ya, doing?', '00000019Hey, how ya, doing?'),
-    ('James Feore', '00000011James Feore'),
-    ('Ophelia Yin', '00000011Ophelia Yin'),
-    ('Testing testing testing', '00000023Testing testing testing'),
-    ('Here is a much longer one to test with. It\'s super long.', '00000056Here is a much longer one to test with. It\'s super long.')
-]
-
-
 @pytest.mark.parametrize('msg', TEST_ECHO)
 def test_client(msg):
     """Take a msg,send it, return that same message."""
     from client import client
-    assert client(msg) == 'HTTP/1.1 200 OK'
+    assert client(msg) == 'HTTP/1.1 200 OK\r\n\r\n'
 
 
 def test_response_ok():
     """Take a msg,send it, return that same message."""
     from server import response_ok
-    assert response_ok() == 'HTTP/1.1 200 OK'
+    assert response_ok() == b'HTTP/1.1 200 OK\r\n\r\n'
 
 
 def test_response_error():
     """Take a msg,send it, return that same message."""
     from server import response_error
-    assert response_error() == 'HTTP/1.1 500 Internal Server Error'
-
-
-@pytest.mark.parametrize('msg, result', TEST_APPEND)
-def test_append_len(msg, result):
-    """Take a msg and add its length in front of it."""
-    from client import append_len
-    assert append_len(msg) == result
+    assert response_error() == b'HTTP/1.1 500 Internal Server Error\r\n\r\n'
